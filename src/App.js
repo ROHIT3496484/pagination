2393d3a2bb5f05dbb699e304fd62de9924a31ee2
ROHIT3496484 <rohitrana349@gmail.com>
2025-05-05 14:02:51 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Poostpagination} from './Component/Pagination/Poostpagination';
+
+
+
+
 
 function App() {
+  /**const [val, setVal] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+
+  const fetchval = async () => {
+    const res = await fetch("https://dummyjson.com/products?limit=100");
+    const data = await res.json();
+    if (data && data.products) {
+      setVal(data.products);
+    }
+  };
+
+  useEffect(() => {
+    fetchval();
+  }, []);
+
+  // Pagination logic
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = val.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);**/
+
   return (
     <div className="App">
-      
+      {/**<h1>Pagination</h1>
+      <div className="product-grid">
+        {currentItems.length > 0 &&
+          currentItems.map((mp) => (
+            <span key={mp.id}>
+              <img src={mp.thumbnail} alt={mp.title} />
+              <p>{mp.title}</p>
+            </span>
+          ))}
+      </div>
+      <Page
+        itemsPerPage={itemsPerPage}
+        totalItems={val.length}
+        paginate={paginate}
+        currentPage={currentPage}
+      />**/}
+      <Poostpagination/>
     </div>
   );
 }
 
-export default App;
+export default App();
---
+++ src/Component/Pagination/Poostpagination.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Pagination from './Pagination'
+
+export const Poostpagination = () => {
+  return (
+    <div>Poostpagination
+        <div><Pagination/></div>
+    </div>
+  )
+}
+
